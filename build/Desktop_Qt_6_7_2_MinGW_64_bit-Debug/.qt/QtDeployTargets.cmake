9d02876f04e479b78173d9e1c08cdc92399c4e98
Simpidbit <8155530@gmail.com>
2024-10-10 23:25:28 +0800

    首次提交

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_bloster_FILE E:/code/program/bloster/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/bloster.exe)
+set(__QT_DEPLOY_TARGET_bloster_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_bloster_RUNTIME_DLLS D:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "main_window_t.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "bloster_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    main_window_t w;
+    w.show();
+    return a.exec();
+}
